d1ae9ac5329f619e5f92d27831f9547d990ab23c
Beccall <33158964+Beccall@users.noreply.github.com>
2018-07-07 07:17:21 -0500

    Update Yahtzee.py

--- Yahtzee.py
+++ Yahtzee.py
@@ -2,36 +2,20 @@ from random import randint
 
 """Yahtzee Game"""
 
-#Dice animation function
 total_dice = [] 
-def dice(num):
-	
-	if num == 1:
-		print(" ___\n|.  |\n|   |\n ---")
-		
-	if num == 2:
-		print(" ___\n|.. |\n|   |\n ---")
-		
-	if num == 3:
-		print(" ___\n|...|\n|   |\n ---")
-		
-	if num == 4:
-		print(" ___\n|...|\n|.  |\n ---")
-		
-	if num == 5:
-		print(" ___\n|...|\n|.. |\n ---")
-		
-	if num == 6:
-		print(" ___\n|...|\n|...|\n ---")
 
+one_dice = " ___\n|.  |\n|   |\n ---"
+two_dice = " ___\n|.. |\n|   |\n ---"
+three_dice = " ___\n|...|\n|   |\n ---"
+four_dice = " ___\n|...|\n|.  |\n ---"
+five_dice = " ___\n|...|\n|.. |\n ---"
+six_dice = " ___\n|...|\n|...|\n ---"
 
 for i in range(5):
 	dice_face = randint(1,6)
 	total_dice.append(dice_face)
-	dice(dice_face)
+
 print(total_dice)
-
-
 
 one = 0
 two = 0
@@ -39,46 +23,62 @@ three = 0
 four = 0
 five = 0
 
-	
-for i in range(5):
+number_of_dice = 5	
+for i in range(number_of_dice):
 	if total_dice[i] == 1:
+		print(one_dice)
 		one = one + 1
-for i in range(5):
+
+for i in range(number_of_dice):
 	if total_dice[i] == 2:
+		print(two_dice)
 		two = two + 1
-for i in range(5):
+		
+for i in range(number_of_dice):
 	if total_dice[i] == 3:
+		print(three_dice)
 		three = three + 1
-for i in range(5):
+		
+for i in range(number_of_dice):
 	if total_dice[i] == 4:
+		print(four_dice)
 		four = four + 1
-for i in range(5):
+		
+for i in range(number_of_dice):
 	if total_dice[i] == 5:
+		print(five_dice)
 		five = five + 1
-	
+		
+
 print("\n  One | " + str(one))
 print("  Two | " + str(two))
 print("Three | " + str(three))
 print(" Four | " + str(four))
 print(" Five | " + str(five))
 
-def length_counter(dice_number):
-	if dice_number >= 3:
-		print("Do you want to keep all the dice with the number " + str(dice_number) + "? ")
-	
-length_counter(one)
-length_counter(two)
-length_counter(three)
-length_counter(four)
-length_counter(five)
 
-re_roll = input("How many dice do you want to re-roll? ")
-new_roll = []
-for i in range(int(re_roll)):
-	dice_face = randint(1,6)
-	new_roll.append(dice_face)
-	dice(dice_face)
-print(new_roll)
-
-
-	
+number_of_dice = input("How many dice do you want to re-roll? ")
+for i in range(int(number_of_dice)):
+	if total_dice[i] == 1:
+		print(one_dice)
+		one = one + 1
+		
+for i in range(int(number_of_dice)):
+	if total_dice[i] == 2:
+		print(two_dice)
+		two = two + 1
+		
+for i in range(int(number_of_dice)):
+	if total_dice[i] == 3:
+		print(three_dice)
+		three = three + 1
+		
+for i in range(int(number_of_dice)):
+	if total_dice[i] == 4:
+		print(four_dice)
+		four = four + 1
+		
+for i in range(int(number_of_dice)):
+	if total_dice[i] == 5:
+		print(five_dice)
+		five = five + 1
